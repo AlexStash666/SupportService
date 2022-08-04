60a63217a42fb721270f6695f113cc82dc55376b
AlexStash666 <54244203+AlexStash666@users.noreply.github.com>
2022-08-04 12:07:36 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 celery -A config  worker -l info
+python manage.py collectstatic
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
